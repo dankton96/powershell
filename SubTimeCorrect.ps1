30b11eca7c2936dd97d4538cbdf585701c5ecbe2
dankton96 <dankton96@hotmail.com>
2020-01-09 13:24:19 +0000

    Implemented srt file selection

--- SubTimeCorrect.ps1
+++ SubTimeCorrect.ps1
@@ -1,13 +1,13 @@
-Add-Type -AssemblyName 'Windows.Forms'
+Add-Type -AssemblyName 'System.Windows.Forms'
 $openFileDialog = New-Object windows.forms.openfiledialog   
 $openFileDialog.initialDirectory = [System.IO.Directory]::GetCurrentDirectory()   
-$openFileDialog.title = "Select PublishSettings Configuration File to Import"   
+$openFileDialog.title = "Select the subtitle file:"   
 $openFileDialog.filter = "All files (*.*)| *.*"   
 $openFileDialog.ShowHelp = $True   
 $result = $openFileDialog.ShowDialog()   # Display the Dialog / Wait for user response 
     # in ISE you may have to alt-tab or minimize ISE to see dialog box
-#$FilePath=$OpenFileDialog.filename
-#echo $FilePath
+$FilePath=$OpenFileDialog.filename
+echo $FilePath
 #    $result 
 #    if($result -eq "OK")    {    
 #            Write-Host "Selected Downloaded Settings File:"  -ForegroundColor Green  
