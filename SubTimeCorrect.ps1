46be88feb001a89c712e73abf5ad8352771ca570
dankton96 <dankton96@hotmail.com>
2020-01-10 11:27:39 +0000

    implementing subtitle correction

--- SubTimeCorrect.ps1
+++ SubTimeCorrect.ps1
@@ -11,8 +11,11 @@ $FilePath=$OpenFileDialog.filename
 $lines2calc=Get-Content $FilePath | Select-string -Pattern "\d\d:\d\d:\d\d\,\d\d\d --> \d\d:\d\d:\d\d\,\d\d\d"
 #$lines2calc.GetType()
 #echo $lines2calc.LineNumber
-ChangeLine=[n for n in $lines2calc.LineNumber]
-#foreach ($l in )
-#{
-#	echo 
-#}
+#ChangeLine=[$lines2calc.LineNumber]
+foreach ($l in $lines2calc.LineNumber)
+{
+#	echo $l.type
+	$t1=$lines2calc[$l].substring(0,12)
+	$t2=$lines2calc[$l].substring(17)
+	echo $t1+'\t'+$t2
+}
