3e1a54bc862754156b315091799bf809034847e4
dankton96 <dankton96@hotmail.com>
2020-01-12 00:17:02 +0000

    corrected subtitle position detection to change

--- SubTimeCorrect.ps1
+++ SubTimeCorrect.ps1
@@ -7,15 +7,23 @@ $openFileDialog.filter = "All files (*.*)| *.*"
 $openFileDialog.ShowHelp = $True   
 $result = $openFileDialog.ShowDialog()
 $FilePath=$OpenFileDialog.filename
+$FileContent=Get-Content $FilePath
+#echo $FileContent
 #echo $FilePath
 $lines2calc=Get-Content $FilePath | Select-string -Pattern "\d\d:\d\d:\d\d\,\d\d\d --> \d\d:\d\d:\d\d\,\d\d\d"
-#$lines2calc.GetType()
-#echo $lines2calc.LineNumber
-#ChangeLine=[$lines2calc.LineNumber]
-foreach ($l in $lines2calc.LineNumber)
+#echo $lines2calc.GetType()
+#echo ($lines2calc.LineNumber)
+foreach ($l in $lines2calc)
 {
-#	echo $l.type
-	$t1=$lines2calc[$l].substring(0,12)
-	$t2=$lines2calc[$l].substring(17)
-	echo $t1+'\t'+$t2
+#	echo $FileContent[$l.LineNumber-1]
+	$t1=$FileContent[$l.LineNumber-1].substring(0,12)
+	$t2=$FileContent[$l.LineNumber-1].substring(17)
+	echo ($t1+'\t'+$t2)
 }
+#foreach ($l in $lines2calc)
+#{
+#	$s=ToString($lines2calc[$l])
+#	$t1=$s.substring(0,12)
+#	$t2=$s.substring(17)
+#	echo $t1+'\t'+$t2
+#}
