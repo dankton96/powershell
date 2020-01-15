2ca5e5ecaca684a5215bb8951f09a801e3ec052b
dankton96 <dankton96@hotmail.com>
2020-01-15 22:01:23 +0000

    Update SubTimeCorrect.ps1

--- SubTimeCorrect.ps1
+++ SubTimeCorrect.ps1
@@ -21,7 +21,11 @@ foreach ($l in $lines2calc)
 	#10/01/1000 00:40:13,372
 	#$t1=Get-Date -Date(([Int32]$t1s.substring(6,4)),([Int32]$t1s.substring(3,2)),([Int32]$t1s.substring(0,2)),([Int32]$t1s.substring(11,2)),([Int32]$t1s.substring(14,2)),([Int32]$t1s.substring(17,2)),([Int32]$t1s.substring(20,3)))
 	#$t2=Get-Date -Date(([Int32]$t2s.substring(6,4)),([Int32]$t2s.substring(3,2)),([Int32]$t2s.substring(0,2)),([Int32]$t2s.substring(11,2)),([Int32]$t2s.substring(14,2)),([Int32]$t2s.substring(17,2)),([Int32]$t2s.substring(20,3)))
-	$t1=Get-Date -Date $t1s.substring(0,18)
-	$t2=Get-Date -Date $t2s.substring(0,18)
-	echo $t1'------'$t2
+	#$t1=Get-Date -Date $t1s.substring(0,18)
+	$t1=Get-Date -Day $t1s.substring(0,2) -Month $t1s.substring(3,2) -Year $t1s.substring(6,4) -Hour $t1s.substring(11,2) -Minute $t1s.substring(14,2) -Second $t1s.substring(17,2) -Millisecond $t1s.substring(20,3)
+	#$t2=Get-Date -Date $t2s.substring(0,18)
+	$t2=Get-Date -Day $t2s.substring(0,2) -Month $t2s.substring(3,2) -Year $t2s.substring(6,4) -Hour $t2s.substring(11,2) -Minute $t2s.substring(14,2) -Second $t2s.substring(17,2) -Millisecond $t2s.substring(20,3)
+	#$t1.AddMilliseconds([Int32]$t1s.substring(20,3))
+	#$t2.AddMilliseconds([Int32]$t2s.substring(20,3))
+	echo $t1 -Format FileDateTime '------'$t2 -Format FileDateTime 
 }
